11a7bf1691ad2204c232e9beb73fe5196f87ffaf
IIKHAIRYII <kkhairy2144@gmail.com>
2024-11-20 17:54:08 +0200

    push latest

--- Demo.Presentation/Pages/Customer/Create.cshtml.cs
+++ Demo.Presentation/Pages/Customer/Create.cshtml.cs
@@ -1,12 +1,27 @@
+using Demo.Application.DTOs.Customer.Requests;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Demo.Presentation.Pages.Customer
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public AddCustomerDto Customer { get; set; }
+
+        private readonly IMediator _mediator;
+        public CreateModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
         public void OnGet()
         {
+        }
+        public async Task OnPost()
+        {
+            await _mediator.Send(Customer);
         }
     }
 }
